011902f486a4b7db972ad325dce67a91698865d8
Stalin <superldu91@gmail.com>
2025-07-27 00:27:45 -0500

    Inicio del Desarrollo de 2 microservicios

---
+++ cuenta_movimiento/src/test/java/com/remeco/cuenta_movimiento/CuentaMovimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remeco.cuenta_movimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaMovimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
